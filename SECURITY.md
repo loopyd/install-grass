ada0f169cf8ccb71ed3166067def38099fba4b8e
loopyd <nightwintertooth@gmail.com>
2024-07-26 05:52:26 -0700

    Patch for SSL certificates.

--- SECURITY.md
+++ SECURITY.md
@@ -2,13 +2,26 @@
 
 This repository was created by a **community contributor**, and not officially endorsed by Wyrd Network or the project team.
 
-Checksums for:  ``grass_4.26.0_amd64.deb`` are provided for your reference:
+See: [manifest.json](./manifest.json) for the installer script manifest.  This file is directly responsible for downloading the Grass Desktop node and relevant dependencies from secure sources.
+
+The contents of that file should match the values in this security declarence.
+
+If they do not, please notify the maintainers, **immediately**, and refrain from using this installer.
+
+Checksums for:  ``grass_4.26.0_amd64.deb``:
 
 | Type | Checksum |
 | ---- | -------- |
-| **md5** | ``1216fa71e80f7e4ad3e9591fbd61a984`` |
-| **sha256** | ``76119d415a021893d033dfc19e7b44cceedbaa7cea5f23a8d56e82abde188cd9`` |
-| **crc32** | ``cfa14a38`` |
+| **md5** | ``b4e35e48235059a8d1ddf72666dbacbd`` |
+| **sha256** | ``6dcfa7b73e4ae5f3552494f8589bfabb68d11cbf29862797ab8f303800056ca6`` |
+| **crc32** | ``91be8426`` |
+
+Checksums for: ``SectigoRSADomainValidationSecureServerCA.crt``:
+
+| Type | Checksum |
+| **md5** | ``bb3a7603202c96d0580c3bf3eb3ee3f2`` |
+| **sha256** | ``3441f78cfe18b12309e418774eac6d05cd3a074ee4187a652712f777fad0c9d5`` |
+| **crc32** | ``46a9649b`` |
 
 This repository was created by a member of the Grass OGs / Grass community contributors:  **deitydurg**.
 
--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-CSCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-GRASS_DEB_DOWNLOAD_DIR="${CSCRIPT_DIR}/grass_4.26.0_amd64.deb"
-GRASS_DOWNLOAD_DIR="${CSCRIPT_DIR}/grass-node"
-GRASS_MD5="1216fa71e80f7e4ad3e9591fbd61a984"
-GRASS_SHA256="76119d415a021893d033dfc19e7b44cceedbaa7cea5f23a8d56e82abde188cd9"
-GRASS_CRC32="cfa14a38"
-INSTALL_PREFIX=${INSTALL_PREFIX:-"/usr"}
-USER_MODE=${USER_MODE:-0}
+# install-grass: Install Grass on Any Linux!
+#
 
 if [ "$(id -u)" -ne 0 ]; then
-	echo "This installer must not be run as root as it will make changes to file ownership and install files as nessecary on your system." >&2
+	echo "This installer must not be run as root as it will make changes to file ownership and install files as necessary on your system." >&2
 	exit 1
 fi
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
@@ -23,32 +17,151 @@ else
 	USER_NAME=${USER_NAME:-$(whoami)}
 fi
 
-function __download_gdrive() {
+CSCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CONFIG_FILE=${CONFIG_FILE:-"manifest.json"}
+INSTALL_PREFIX=${INSTALL_PREFIX:-"/usr"}
+USER_MODE=${USER_MODE:-0}
+DEBUG=${DEBUG:-0}
+LOGGING=${LOGGING:-1}
+LOG_FILE=${LOG_FILE:-"/var/log/grass-install.log"}
+
+# Colors
+C_RED=$(tput setaf 1)
+C_GREEN=$(tput setaf 2)
+C_YELLOW=$(tput setaf 3)
+C_BLUE=$(tput setaf 4)
+C_MAGENTA=$(tput setaf 5)
+C_CYAN=$(tput setaf 6)
+C_RESET=$(tput sgr0)
+C_BOLD=$(tput bold)
+C_UNDERLINE=$(tput smul)
+
+####################################################################################################
+# Logging functions
+####################################################################################################
+
+function __strip_ansi() {
+	sed -r "s/\x1B\[[0-9;]*[JKmsu]//g" <<<"$1"
+}
+function __info() {
+	local _logstr _logstr_noansi
+	_logstr="${C_CYAN}[${C_BOLD}INFO${C_RESET}${C_CYAN}] $1${C_RESET}"
+	_logstr_noansi=$(__strip_ansi "${_logstr}")
+	echo -e "${_logstr}" >&2
+	if [ ${LOGGING} -eq 1 ]; then
+		echo -e "$(date -I) ${_logstr_noansi}" >>"${LOG_FILE}"
+	fi
+}
+function __warn() {
+	local _logstr _logstr_noansi
+	_logstr="${C_YELLOW}[${C_BOLD}WARN${C_RESET}${C_YELLOW}] $1${C_RESET}"
+	_logstr_noansi=$(__strip_ansi "${_logstr}")
+	echo -e "${_logstr}" >&2
+	if [ ${LOGGING} -eq 1 ]; then
+		echo -e "$(date -I) ${_logstr_noansi}" >>"${LOG_FILE}"
+	fi
+}
+function __error() {
+	local _logstr _logstr_noansi
+	_logstr="${C_RED}[${C_BOLD}ERROR${C_RESET}${C_RED}] $1${C_RESET}"
+	_logstr_noansi=$(__strip_ansi "${_logstr}")
+	echo -e "${_logstr}" >&2
+	if [ ${LOGGING} -eq 1 ]; then
+		echo -e "$(date -I) ${_logstr_noansi}" >>"${LOG_FILE}"
+	fi
+}
+function __success() {
+	local _logstr _logstr_noansi
+	_logstr="${C_GREEN}[${C_BOLD}SUCCESS${C_RESET}${C_GREEN}] $1${C_RESET}"
+	_logstr_noansi=$(__strip_ansi "${_logstr}")
+	echo -e "${_logstr}" >&2
+	if [ ${LOGGING} -eq 1 ]; then
+		echo -e "$(date -I) ${_logstr_noansi}" >>"${LOG_FILE}"
+	fi
+}
+function _exit_trap() {
+	local _exit_code=$?
+	if [ ${_exit_code} -eq 0 ]; then
+		__success "Installation completed successfully"
+	else
+		__error "Installation failed with exit(${_exit_code}).  Please use the log located at: ${LOG_FILE} when submitting a bug report."
+	fi
+	return ${_exit_code}
+}
+trap _exit_trap EXIT ERR SIGINT SIGTERM
+
+####################################################################################################
+# DeiLib installer manifest library
+####################################################################################################
+
+function __download_file_gdrive() {
 	local _file_id=$1
 	if [ -z "${_file_id}" ]; then
-		echo "File ID not provided" >&2
+		__error "File ID not provided"
 		return 1
 	fi
 	shift 1
 	local _output_file=$1
 	if [ -z "${_output_file}" ]; then
-		echo "Output file not provided" >&2
+		__error "Output file not provided"
 		return 1
 	fi
 	if [ -f "${_output_file}" ]; then
-		echo "Output file already exists: ${_output_file}" >&2
+		__warn "Output file already exists: ${_output_file}"
 		return 0
 	fi
-	local _tmpfile=$(mktemp /tmp/gdrive.XXXXXX)
-	local _url=
-	local _confirm=$(wget --quiet --save-cookies $_tmpfile --keep-session-cookies --no-check-certificate --content-disposition "https://drive.google.com/uc?export=download&id=${_file_id}" -O-)
-	local _conf=$(echo $_confirm | sed -n 's/.*name="id" value="\([^"]*\)".*/\1/p')
-	local _uuid=$(echo $_confirm | sed -n 's/.*name="uuid" value="\([^"]*\)".*/\1/p')
-	echo "Downloading file: ${_file_id}..."
-	wget --quiet --load-cookies $_tmpfile "https://drive.usercontent.google.com/download?id=${_file_id}&export=download&confirm=t&uuid=${_uuid}" -O ${_output_file} || {
-		echo "Failed to download file" >&2
+	local _tmpfile _url _confirm _conf _uuid _download_dir
+	_tmpfile=$(mktemp /tmp/gdrive.XXXXXX) 
+	_confirm=$(wget --quiet --save-cookies "${_tmpfile}" --keep-session-cookies --no-check-certificate --content-disposition "https://drive.google.com/uc?export=download&id=${_file_id}" -O-)
+	_conf=$(echo "${_confirm}" | sed -n 's/.*name="id" value="\([^"]*\)".*/\1/p')
+	_uuid=$(echo "${_confirm}" | sed -n 's/.*name="uuid" value="\([^"]*\)".*/\1/p')
+	_download_dir="${_output_file%/*}"
+	if [ ! -d "${_download_dir}" ]; then
+		if ! mkdir -p "${_download_dir}"; then
+			__error "Failed to create download destination directory: ${_download_dir}"
+			return 1
+		fi
+	fi
+	wget --quiet --load-cookies "${_tmpfile}" "https://drive.usercontent.google.com/download?id=${_file_id}&export=download&confirm=t&uuid=${_uuid}" -O ${_output_file} || {
+		__error "Failed to download file"
 		return 1
 	}
+	rm -f "${_tmpfile}" || {
+		__warn "Failed to remove temporary file: ${_tmpfile}"
+	}
+	__success "File downloaded to: ${_output_file}"
+	return 0
+}
+
+function __download_file_direct() {
+	local _file_url=$1
+	if [ -z "${_file_url}" ]; then
+		__error "File URL not provided"
+		return 1
+	fi
+	shift 1
+	local _output_file=$1
+	if [ -z "${_output_file}" ]; then
+		__error "Output file not provided"
+		return 1
+	fi
+	if [ -f "${_output_file}" ]; then
+		__warn "Output file already exists: ${_output_file}"
+		return 0
+	fi
+	_download_dir="${_output_file%/*}"
+	if [ ! -d "${_download_dir}" ]; then
+		if ! mkdir -p "${_download_dir}"; then
+			__error "Failed to create download destination directory: ${_download_dir}"
+			return 1
+		fi
+	fi
+	__info "Downloading file: ${_file_url}..."
+	wget --quiet --no-check-certificate "${_file_url}" -O "${_output_file}" || {
+		__error "Failed to download file"
+		return 1
+	}
+	__success "File downloaded to: ${_output_file}"
 	return 0
 }
 
@@ -62,11 +175,11 @@ function __check_command() {
 function __check_file() {
 	local _file_path=$1
 	if [ -z "${_file_path}" ]; then
-		echo "File path not provided" >&2
+		__error "File path not provided"
 		return 1
 	fi
 	if [ ! -f "${_file_path}" ]; then
-		echo "File not found: ${_file_path}" >&2
+		__error "File not found: ${_file_path}"
 		return 1
 	fi
 	shift 1
@@ -76,11 +189,11 @@ function __check_file() {
 	local _md5_checksum=$1
 	if [ -n "${_md5_checksum}" ]; then
 		if ! __check_command md5sum; then
-			echo "md5sum utility not found, could not verify checksum of file" >&2
+			__error "md5sum utility not found, could not verify checksum of file"
 			return 1
 		fi
 		if [ "$(md5sum ${_file_path} | awk '{print $1}')" != "${_md5_checksum}" ]; then
-			echo "MD5 checksum does not match" >&2
+			__error "MD5 checksum does not match"
 			return 1
 		fi
 	fi
@@ -91,11 +204,11 @@ function __check_file() {
 	local _sha256_checksum=$1
 	if [ -n "${_sha256_checksum}" ]; then
 		if ! __check_command sha256sum; then
-			echo "sha256sum utility not found, could not verify checksum of file" >&2
+			__error "sha256sum utility not found, could not verify checksum of file"
 			return 1
 		fi
 		if [ "$(sha256sum ${_file_path} | awk '{print $1}')" != "${_sha256_checksum}" ]; then
-			echo "SHA256 checksum does not match" >&2
+			__error "SHA256 checksum does not match"
 			return 1
 		fi
 	fi
@@ -106,15 +219,16 @@ function __check_file() {
 	local _crc32_checksum=$1
 	if [ -n "${_crc32_checksum}" ]; then
 		if ! __check_command crc32; then
-			echo "crc32 utility not found, could not verify checksum of file" >&2
+			__error "crc32 utility not found, could not verify checksum of file"
 			return 1
 		fi
 		if [ "$(crc32 ${_file_path} | awk '{print $1}')" != "${_crc32_checksum}" ]; then
-			echo "CRC32 checksum does not match" >&2
+			__error "CRC32 checksum does not matchg given: $(crc32 ${_file_path} | awk '{print $1}') expected: ${_crc32_checksum}"
 			return 1
 		fi
 	fi
 	shift 1
+	__success "File: ${_file_path} checksums verified successfully"
 	return 0
 }
 
@@ -122,46 +236,46 @@ function __extract_deb_package() {
 	local _deb_file=$1
 	local _target_dir=$2
 	if [ -z "${_deb_file}" ] || [ -z "${_target_dir}" ]; then
-		echo "Usage: __extract_deb_package <deb_file> <target_dir>" >&2
+		__error "Usage: __extract_deb_package <deb_file> <target_dir>"
 		return 1
 	fi
 	if [ ! -f "${_deb_file}" ]; then
-		echo "File not found: ${_deb_file}" >&2
+		__error "File not found: ${_deb_file}"
 		return 1
 	fi
 	if [ ! -d "${_target_dir}" ]; then
 		if ! mkdir -p "${_target_dir}"; then
-			echo "Failed to create target directory: ${_target_dir}" >&2
+			__error "Failed to create target directory: ${_target_dir}"
 			return 1
 		fi
 	fi
 	if ! __check_command ar; then
-		echo "ar command not found" >&2
+		__error "ar command not found"
 		return 1
 	fi
 	if ! __check_command tar; then
-		echo "tar command not found" >&2
+		__error "tar command not found"
 		return 1
 	fi
 	pushd "${_target_dir}" &>/dev/null || return 1
 	ar x "${_deb_file}" || {
-		echo "Failed to extract ${_deb_file} package" >&2
+		__error "Failed to extract ${_deb_file} package"
 		return 1
 	}
 	tar -xf control.tar.* || {
-		echo "Failed to extract control.tar.*" >&2
+		__error "Failed to extract control.tar.*"
 		return 1
 	}
 	tar -xf data.tar.* || {
-		echo "Failed to extract data.tar.*" >&2
+		__error "Failed to extract data.tar.*"
 		return 1
 	}
 	rm control.tar.* data.tar.* || {
-		echo "Failed to remove control.tar.* data.tar.*" >&2
+		__error "Failed to remove control.tar.* data.tar.*"
 		return 1
 	}
 	popd &>/dev/null || return 1
-	echo "Package extracted to ${_target_dir}"
+	__success "Package extracted to ${_target_dir}"
 	return 0
 }
 
@@ -170,7 +284,7 @@ function __install_files() {
     shift 1
     local _destination_folder=$1
     if [ -z "${_destination_folder}" ]; then
-        echo "Destination folder not provided" >&2
+        __error "Destination folder not provided"
         return 1
     fi
     shift 1
@@ -181,49 +295,51 @@ function __install_files() {
 	fi
     shift 1
     if [ -z "${_base_folder}" ]; then
-        echo "Base folder not provided" >&2
+        __error "Base folder not provided"
         return 1
     fi
     if [ ! -d "${_base_folder}" ]; then
-        echo "Base folder not found: ${_base_folder}" >&2
+        __error "Base folder not found: ${_base_folder}"
         return 1
     fi
     if [ ! -d "${_destination_folder}" ]; then
         if ! mkdir -p "${_destination_folder}"; then
-            echo "Failed to create destination folder: ${_destination_folder}" >&2
+            __error "Failed to create destination folder: ${_destination_folder}"
             return 1
         fi
     fi
     find "${_base_folder}/usr" -type d | while read -r dir; do
         dest_dir="${_destination_folder}${dir#${_base_folder}/usr}"
         if [ ! -d "${dest_dir}" ]; then
-            mkdir -p "${dest_dir}"
+            mkdir -p "${dest_dir}" || {
+				__error "Failed to create folder: ${dest_dir}"
+			}
         fi
     done
     find "${_base_folder}/usr" -type f | while read -r file; do
         dest_file="${_destination_folder}${file#${_base_folder}/usr}"
         cp "${file}" "${dest_file}" || {
-            echo "Failed to copy ${file} to ${dest_file}" >&2
+            __error "Failed to copy ${file} to ${dest_file}"
             return 1
         }
         if [ $_usermode -eq 1 ]; then
             chown $(whoami):$(whoami) "${dest_file}" || {
-                echo "Failed to change ownership of ${dest_file}" >&2
+                __error "Failed to change ownership of ${dest_file}"
                 return 1
             }
         fi
 		chmod 0755 "${dest_file}" || {
-			echo "Failed to change permissions of ${dest_file}" >&2
+			__error "Failed to change permissions of ${dest_file}"
 			return 1
 		}
     done
     echo "Files copied to ${_destination_folder}"
 	if ! __check_command update-desktop-database; then
-		echo -e "update-desktop-database utility not found, please update your xdg-utils package and run:\n\nupdate-desktop-database ~/.local/share/applications if you'd like grass to be in your session menu." >&2
+		__warning -e "update-desktop-database utility not found, please update your xdg-utils package and run:\n\nupdate-desktop-database ~/.local/share/applications if you'd like grass to be in your session menu."
 	else
 		# run the command as the user
 		/bin/su -c "update-desktop-database ~/.local/share/applications" -s /bin/bash $USER_NAME || {
-			echo "Failed to update desktop database" >&2
+			echo "Failed to update desktop database"
 			return 1
 		}
 	fi
@@ -240,7 +356,133 @@ function __install_files() {
     fi
 }
 
-__download_gdrive "1acd81bX1Gv8JOU_NqzYkxcg_76H9CGUW" "${GRASS_DEB_DOWNLOAD_DIR}" || exit $?
-__check_file "${GRASS_DEB_DOWNLOAD_DIR}" "${GRASS_MD5}" "${GRASS_SHA256}" "${GRASS_CRC32}" || exit $?
-__extract_deb_package "${GRASS_DEB_DOWNLOAD_DIR}" "${GRASS_DOWNLOAD_DIR}" || exit $?
-__install_files "${GRASS_DOWNLOAD_DIR}" "${INSTALL_PREFIX}" "${USER_MODE}" || exit $?
+function __install_cert() {
+	local _cert_file=$1
+	if [ -z "${_cert_file}" ]; then
+		__error "Certificate file not provided"
+		return 1
+	fi
+	if [ ! -f "${_cert_file}" ]; then
+		__error "Certificate file not found: ${_cert_file}"
+		return 1
+	fi
+	if ! __check_command update-ca-certificates; then
+		__error "update-ca-certificates command not found"
+		return 1
+	fi
+	cp "${_cert_file}" /etc/ssl/certs/ || {
+		__error "Failed to copy certificate file to /etc/ssl/certs/"
+		return 1
+	}
+	c_rehash /etc/ssl/certs/ || {
+		__error "Failed to update certificate hash"
+		return 1
+	}
+	update-ca-certificates || {
+		__error "Failed to update certificate store"
+		return 1
+	}
+	__success "Certificate installed successfully"
+}
+
+function __process_node() {
+	local _file_name=$1
+	if [ -z "${_file_name}" ]; then
+		__error "File name not provided"
+		return 1
+	fi
+	shift 1
+	if [ $# -eq 0 ]; then
+		__error "Manifest file not provided"
+		return 1
+	fi
+	local _config_file=$1
+	if [ -z "${_config_file}" ]; then
+		__error "Manifest file not provided"
+		return 1
+	fi
+	if [ ! -f "${_config_file}" ]; then
+		__error "Manifest file not found: ${_config_file}"
+		return 1
+	fi
+	local _file_info _file_id _md5 _sha256 _crc32 _output_file _download_dir
+	declare -a _actions
+	_file_info=$(jq -r --arg file_name "$_file_name" '.[$file_name]' "${_config_file}")
+	_file_identifier=$(echo "${_file_info}" | jq -r '.file_identifier')
+	if [ -z "${_file_identifier}" ]; then
+		__error "File identifier not found in manifest"
+		return 1
+	fi
+	_md5=$(echo "${_file_info}" | jq -r '.md5')
+	if [ -z "${_md5}" ]; then
+		__error "MD5 checksum not found in manifest for file: ${_file_name}"
+		return 1
+	fi
+	_sha256=$(echo "${_file_info}" | jq -r '.sha256')
+	if [ -z "${_sha256}" ]; then
+		__error "SHA256 checksum not found in manifest for file: ${_file_name}"
+		return 1
+	fi
+	_crc32=$(echo "${_file_info}" | jq -r '.crc32')
+	if [ -z "${_crc32}" ]; then
+		__error "CRC32 checksum not found in manifest for file: ${_file_name}"
+		return 1
+	fi
+	_output_file="${CSCRIPT_DIR}/.grass-install/${_file_name}"
+	IFS=' ' mapfile -t _actions <<<$(echo "${_file_info}" | jq -r '.actions[]')
+	if [ ${#_actions[@]} -eq 0 ]; then
+		__error "No installation actions found in manifest for file: ${_file_name}"
+		return 1
+	fi
+	for _action in "${_actions[@]}"; do
+		case "${_action}" in
+			"download_gdrive")
+				__info "Downloading file: ${_file_name} using gdrive method to ${_output_file}..."
+				__download_file_gdrive "${_file_identifier}" "${_output_file}" || return 1
+				;;
+			"check")
+				__info "Checking file: ${_output_file}..."
+				__check_file "${_output_file}" "${_md5}" "${_sha256}" "${_crc32}" || return 1
+				;;
+			"extract_deb")
+				local _destination_dir
+				_destination_dir="${CSCRIPT_DIR}/.grass-install/${_file_name%.*}"
+				__info "Extracting deb package: ${_output_file} to ${_destination_dir}..."
+				__extract_deb_package "${_output_file}" "${_destination_dir}" || return 1
+				;;
+			"install_deb_folder")
+				local _source_dir
+				_source_dir="${CSCRIPT_DIR}/.grass-install//${_file_name%.*}"
+				__info "Installing deb package folder: ${_source_dir} to ${INSTALL_PREFIX}..."
+				__install_files "${_source_dir}" "${INSTALL_PREFIX}" "${USER_MODE}" || return 1
+				;;
+			"install_cert")
+				local _cert_file
+				_cert_file="${CSCRIPT_DIR}/.grass-install/${_file_name}"
+				__info "Installing certificate: ${_cert_file}..."
+				__install_cert "${_cert_file}" || return 1
+				;;
+			*)
+				echo "Invalid action: ${_action}" >&2
+				return 1
+				;;
+		esac
+	done
+}
+
+function __process_manifest() {
+	local _manifest_file=$1
+	if [ -z "${_manifest_file}" ]; then
+		echo "Manifest file not provided" >&2
+		return 1
+	fi
+	if [ ! -f "${_manifest_file}" ]; then
+		echo "Manifest file not found: ${_manifest_file}" >&2
+		return 1
+	fi
+	for file_name in $(jq -r 'keys[]' "${_manifest_file}"); do
+		__process_node "${file_name}" "${_manifest_file}" || exit $?
+	done
+}
+
+__process_manifest "${CONFIG_FILE}"
